1ebda76618b838fd61922db48bc594d672847dbc
ssmahim01 <mahimzayn666@gmail.com>
2025-02-20 18:25:21 +0600

    Successfully connected with MongoDB then Created collection of all tasks

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -6,6 +7,35 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+const uri = process.env.DB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const taskFlowDB = client.db("taskFlowDB");
+    const taskCollection = taskFlowDB.collection("allTasks");
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("Task management is running now...");
