9f31169cc45c025108a0ed6d12e2a2bbd6f8215c
ssmahim01 <mahimzayn666@gmail.com>
2025-02-20 15:05:01 +0600

    Initial backend setup have done with ExpressJS and CORS also MongoDB

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Task management is running now...");
+});
+
+app.listen(port, () => {
+    console.log(`Task Management Backend is running at port ${port}`);
+});
